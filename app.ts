60317366a74a059f21e6f48e6b5dbce4af4ef190
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2020-04-12 17:20:44 +0200

    env

--- app.ts
+++ app.ts
@@ -1,17 +1,20 @@
+import "https://deno.land/x/dotenv/load.ts";
 import { dso } from "dso";
 import Drash from "drash";
 import * as models from "./models/mod.ts";
 import resources from "./api/mod.ts";
 
+const { env } = Deno
+
 async function database() {
   await models
 
   await dso.connect({
-    hostname: "127.0.0.1",
-    username: "root",
-    db: "statisch",
-    password: "hoi123",
-    port: 3306
+    hostname: env().DATABASE_HOST,
+    username: env().DATABASE_USER,
+    db: env().DATABASE_NAME,
+    password: env().DATABASE_PASSWORD,
+    port: parseInt(env().DATABASE_PORT)
   });
 
   // await dso.sync(true)
